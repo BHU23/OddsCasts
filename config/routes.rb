749a7e934c8e25ec9b0b896de3d346088b490eaa
Bhuwadol <bhuwadol.odds@Bhuwadols-MacBook-Air.local>
2024-12-09 10:26:31 +0700

    Adding: admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   # Define CRUD paths for Articles
   resources :articles
 
-  # Health check path
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Custom admin routes
-  get "/admin/new", to: "admin#new", as: :new_admin
-  get "/admin/edit", to: "admin#edit", as: :edit_admin
+  # Admin routes for managing articles
+  namespace :admin do
+    resources :articles
+  end
 
   # Defines the root path route ("/")
-  root "articles#index" # Set articles#index as the root route
+  root "articles#index"
 end
